fa525451f6653953614f33d17a2071d5891d6730
jiale <jiale1810418002@gmail.com>
2025-09-30 22:44:25 -0400

    Prepare for dev upload

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CSE442/2025-Fall/cse-442z/jchen544/',
+  base: '/CSE442/2025-Fall/cse-442z',
 })
